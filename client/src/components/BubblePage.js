c7cf952f70b51998b7b01fa52984bdb02406b8a8
brianaabreu <briana.abreu01@gmail.com>
2020-07-18 12:04:30 -0700

    mvp, css

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,17 +1,33 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
-
-const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+const BubblePage = () => {
+  const [colorList, setColorList] = useState([]);
+  const [refresh, setRefresh] = useState([]);
+  useEffect(() => {
+    fetchColors();
+  }, [refresh]);
+
+  const fetchColors = id => {
+    axiosWithAuth()
+      .get(`http://localhost:5000/api/colors`)
+      .then(res => setColorList(res.data))
+      .catch(err => console.log(err.response));
+  };
+
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList
+        colors={colorList}
+        updateColors={setColorList}
+        refresh={refresh}
+        setRefresh={setRefresh}
+      />
       <Bubbles colors={colorList} />
     </>
   );
